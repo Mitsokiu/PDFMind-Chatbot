6c434f81fface655ce820b0fbc5aaa648dbb830d
Mitsokiu <thuannisan@gmail.com>
2025-06-24 08:21:08 +0700

    final commit

--- modul/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    db = load_db("all-MiniLM-L6-v2-bf16-q4_k.gguf")
-    query = "g. Một chai rượu vang có thể được định giá dựa  trên  yếu tố nào?"
-    result = query_rag(db, query)
-    print(">> Kết quả truy vấn:\n", result)
